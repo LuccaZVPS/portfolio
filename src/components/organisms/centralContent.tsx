1629e88c773539d3ad696c287a6e268f24434fbb
Lucca <luccazvps@gmail.com>
2023-05-15 14:06:29 -0300

    feat: add centralContent component

---
+++ src/components/organisms/centralContent.tsx
@@ -0,0 +1,9 @@
+import { MainContent } from "../molecules/mainContent";
+
+export const CentralContent = () => {
+  return (
+    <div className="grow w-full flex items-center justify-center">
+      <MainContent />
+    </div>
+  );
+};
