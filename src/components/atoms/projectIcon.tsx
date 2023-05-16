46e6e2991acddd948101aa01d7cc893599f2df99
Lucca <luccazvps@gmail.com>
2023-05-16 14:19:51 -0300

    feat: add projectIcon component

---
+++ src/components/atoms/projectIcon.tsx
@@ -0,0 +1,8 @@
+export const ProjectIcon = () => {
+  return (
+    <img
+      className="w-[calc(19px)] rounded-md xl:w-[calc(22px)]"
+      src="https://portfolio-api-self.vercel.app/ts.svg"
+    ></img>
+  );
+};
