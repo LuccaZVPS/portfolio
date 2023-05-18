3ec556dab9e2e4130032410f4701c86768c21a47
Lucca <luccazvps@gmail.com>
2023-05-18 18:34:04 -0300

    feat: change contactBox to a link

--- src/components/molecules/contactBox.tsx
+++ src/components/molecules/contactBox.tsx
@@ -2,8 +2,14 @@ import { IContact } from "../../types/contact";
 import { Paragraph } from "../atoms/paragraph";
 import { SecundaryHeading } from "../atoms/secundaryHeading";
 export const ContactBox = ({ content, img, title }: IContact) => {
+  console.log(content);
   return (
-    <div className="flex flex-col items-center">
+    <a
+      href={content}
+      rel="noreferrer"
+      target="_blank"
+      className="flex flex-col items-center cursor-pointer"
+    >
       <div className="mb-2 flex justify-center items-center bg-primary dark:bg-primary w-12 h-12 rounded-full">
         <img
           className="w-7"
@@ -13,6 +19,6 @@ export const ContactBox = ({ content, img, title }: IContact) => {
       </div>
       <SecundaryHeading>{title}</SecundaryHeading>
       <Paragraph>{content}</Paragraph>
-    </div>
+    </a>
   );
 };
