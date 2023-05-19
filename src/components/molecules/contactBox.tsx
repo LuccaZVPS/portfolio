182fb8415e95d6f9cc101afaafe222e313967a81
Lucca <luccazvps@gmail.com>
2023-05-18 21:42:50 -0300

    refactor: change contactBox component

--- src/components/molecules/contactBox.tsx
+++ src/components/molecules/contactBox.tsx
@@ -3,21 +3,23 @@ import { Paragraph } from "../atoms/paragraph";
 import { SecundaryHeading } from "../atoms/secundaryHeading";
 export const ContactBox = ({ content, img, title }: IContact) => {
   return (
-    <a
-      href={content}
-      rel="noreferrer"
-      target="_blank"
-      className="flex w-[150px] text-center flex-col items-center cursor-pointer lg:w-[300px]"
-    >
-      <div className="mb-2 flex justify-center items-center bg-primary dark:bg-primary w-12 h-12 rounded-full">
-        <img
-          className="w-7"
-          src={`${import.meta.env.VITE_API_DOMAIN}/${img}`}
-          alt=""
-        />
-      </div>
-      <SecundaryHeading>{title}</SecundaryHeading>
+    <div className="flex w-[150px] text-center flex-col items-center  lg:w-[300px]">
+      <a
+        href={content}
+        rel="noreferrer"
+        className="text-center flex flex-col items-center "
+      >
+        <div className="mb-2 flex justify-center items-center bg-primary dark:bg-primary w-12 h-12 rounded-full">
+          <img
+            className="w-7"
+            src={`${import.meta.env.VITE_API_DOMAIN}/${img}`}
+            alt=""
+          />
+        </div>
+        <SecundaryHeading>{title}</SecundaryHeading>
+      </a>
+
       <Paragraph>{content}</Paragraph>
-    </a>
+    </div>
   );
 };
